6c8e707a05ba9969b6430e7119c7d58a7bb9ece0
Maksym Kovpak <kovpak.maksym@gmail.com>
2024-10-25 20:37:44 +0300

    chore(deps): add shadcn ui library

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,9 @@
 export default defineNuxtConfig({
+  compatibilityDate: '2024-10-25',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
-  css: ['~/assets/css/main.css'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
+  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  shadcn: {
+    prefix: '',
+    componentDir: './components/ui',
   },
 });
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,42 @@
+import animate from 'tailwindcss-animate';
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
+  darkMode: ['class'],
   content: [
-    './components/**/*.{js,vue,ts}',
-    './layouts/**/*.vue',
-    './pages/**/*.vue',
-    './plugins/**/*.{js,ts}',
+    './components/**/*.{ts,tsx,vue}',
+    './layouts/**/*.{ts,tsx,vue}',
+    './pages/**/*.{ts,tsx,vue}',
     './app.vue',
     './error.vue',
   ],
+  prefix: '',
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    extend: {
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+    },
   },
-  plugins: [],
+  plugins: [animate],
 };
 
 export default config;
