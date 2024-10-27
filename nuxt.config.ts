f8fed7452b111b36683c7b9e35d3761e4e3235e7
Maksym Kovpak <kovpak.maksym@gmail.com>
2024-10-27 18:47:51 +0200

    feat: change font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,19 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-10-25',
   devtools: { enabled: false },
-  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  modules: ['@nuxt/fonts', '@nuxt/eslint', '@nuxtjs/tailwindcss', 'shadcn-nuxt'],
   shadcn: {
     prefix: '',
     componentDir: './components/ui',
+  },
+  fonts: {
+    families: [
+      {
+        name: 'Montserrat',
+        provider: 'google',
+        display: 'swap',
+        subsets: ['latin', 'latin-ext', 'cyrillic', 'cyrillic-ext'],
+      },
+    ],
   },
 });
