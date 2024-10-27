d7e5dda908505bfb8bffa6571b872f814c4f1810
Maksym Kovpak <kovpak.maksym@gmail.com>
2024-10-27 17:36:41 +0200

    feat(ui): add button component

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,4 +5,8 @@ import prettier from 'eslint-plugin-prettier';
 
 export default withNuxt({
   plugins: { prettier },
+  rules: {
+    'vue/require-default-prop': 'off',
+    'import/consistent-type-specifier-style': 'warn',
+  },
 });
