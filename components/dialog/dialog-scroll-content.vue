d000bf6af74bdaae46b0c7d4cf42087134f09ac5
Maksym Kovpak <kovpak.maksym@gmail.com>
2024-12-08 17:43:47 +0200

    feat(ui): add dialog component

---
+++ components/dialog/dialog-scroll-content.vue
@@ -0,0 +1,63 @@
+<script lang="ts" setup>
+  import {
+    DialogClose,
+    DialogContent,
+    DialogOverlay,
+    DialogPortal,
+    useForwardPropsEmits,
+  } from 'radix-vue';
+
+  import type { HTMLAttributes } from 'vue';
+  import type { DialogContentEmits, DialogContentProps } from 'radix-vue';
+
+  const props = defineProps<DialogContentProps & { class?: HTMLAttributes['class'] }>();
+  const emits = defineEmits<DialogContentEmits>();
+
+  const delegatedProps = computed(() => {
+    const { class: _, ...delegated } = props;
+
+    return delegated;
+  });
+
+  const forwarded = useForwardPropsEmits(delegatedProps, emits);
+</script>
+
+<template>
+  <DialogPortal>
+    <DialogOverlay
+      class="fixed inset-0 z-50 grid place-items-center overflow-y-auto bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0"
+    >
+      <DialogContent
+        :class="
+          cn(
+            'relative z-50 grid w-full max-w-lg my-8 gap-4 border border-slate-200 bg-white p-6 shadow-lg duration-200 sm:rounded-lg md:w-full',
+            props.class
+          )
+        "
+        v-bind="forwarded"
+        @pointer-down-outside="
+          (event) => {
+            const originalEvent = event.detail.originalEvent;
+            const target = originalEvent.target as HTMLElement;
+
+            if (
+              originalEvent.offsetX > target.clientWidth ||
+              originalEvent.offsetY > target.clientHeight
+            ) {
+              event.preventDefault();
+            }
+          }
+        "
+      >
+        <slot />
+
+        <DialogClose
+          class="absolute top-3 right-3 p-0.5 transition-colors rounded-md hover:bg-slate-100"
+        >
+          <Icon name="lucide:x" size="1rem" />
+          <span class="sr-only">Close</span>
+        </DialogClose>
+      </DialogContent>
+    </DialogOverlay>
+  </DialogPortal>
+</template>
---
+++ components/dialog/dialog-trigger.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+  import { DialogTrigger } from 'radix-vue';
+  import type { DialogTriggerProps } from 'radix-vue';
+
+  const props = defineProps<DialogTriggerProps>();
+</script>
+
+<template>
+  <DialogTrigger v-bind="props">
+    <slot />
+  </DialogTrigger>
+</template>
