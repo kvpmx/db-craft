2819bc352c23f78022ab20fc120cf77d38c5569f
Maksym Kovpak <kovpak.maksym@gmail.com>
2025-01-09 21:52:51 +0200

    fix: do not reset the selected color in the palette component

--- features/diagram/palette.vue
+++ features/diagram/palette.vue
@@ -2,17 +2,17 @@
   import { colors } from '@/lib/constants/colors';
 
   const selectedColor = defineModel<string>({ required: true });
-  // TODO: Require the color to be always selected
 </script>
 
 <template>
-  <ToggleGroup v-model="selectedColor" type="single" class="grid grid-cols-5">
-    <ToggleGroupItem
+  <div class="grid grid-cols-5 gap-1.5">
+    <Button
       v-for="color in colors"
       :key="color"
-      :value="color"
       :style="{ backgroundColor: color }"
+      :data-state="selectedColor === color ? 'on' : 'off'"
       class="h-8 rounded-md border-[1px] border-gray-400 p-0 data-[state=on]:border-2 data-[state=on]:border-black"
-    />
-  </ToggleGroup>
+      @click="selectedColor = color"
+    ></Button>
+  </div>
 </template>
